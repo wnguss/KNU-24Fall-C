ab1d7455c5c5c08d07804b56b36e24e78451e616
wnguss <lgh6603206@gmail.com>
2024-09-19 13:53:47 +0900

    함수 실습

---
+++ day03/day03-1.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int check(a, b);
+
+int main()
+{
+	int num1;
+	int num2 = 1;
+	int result;
+
+	printf("소수인지 확인할 수를 입력하세요 : ");
+	scanf_s("%d", &num1);
+
+	result = check(num1, num2);
+
+	return 0;
+}
+
+int check(a, b)
+{
+	int res = 0;
+
+	if (a / b == a && a / a == 1 && a % 2 != 0 && a % 3 != 0)
+		printf("해당 수는 소수입니다.\n");
+	else
+		printf("해당 수는 소수가 아닙니다.\n");
+
+	return res;
+}
